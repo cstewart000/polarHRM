8a773c4febbe6dcb22967aeeb71719525b018623
cstewart000 <cstewart000@gmail.com>
2017-03-22 20:41:27 +1300

    addedbasic files

---
+++ readInHRMfile.m
@@ -0,0 +1,18 @@
+
+function [ret] = readInHRMfile(path)
+
+
+	File file = fopen(path)
+
+	fileData = textscan(fileId,%s)
+
+
+	% Split the file around the "[HRData]" marker
+
+	fileData
+
+	%dataString = strspilt(fileData, '\n');
+
+	%hrDataValues = strspilt(dataString(2),'\n'))
+
+end
